d1884a7c613a8dc59acfa5827794d33ce707566a
Arvid Alm <arvid.alm@iths.se>
2023-11-09 16:59:41 +0100

    Delete funktioner

--- src/screens/UserList/UserList.tsx
+++ src/screens/UserList/UserList.tsx
@@ -1,103 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
-import { useGetUsersQuery } from '../../store/api/usersApi';
-import { SearchBar, Avatar } from '@rneui/themed';
+import React from 'react';
+import { View, Text, FlatList, RefreshControl, Button } from 'react-native';
+import { ListItem } from '@rneui/themed';
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useUpdateUserMutation, // This hook needs to be created and exported in your usersApi file.
+} from '../../store/api/usersApi';
 
 const UserList = ({ navigation }) => {
-  const { data, isLoading } = useGetUsersQuery({});
-  const [search, setSearch] = useState('');
-  const [filteredData, setFilteredData] = useState([]);
+  const { data, isLoading, refetch } = useGetUsersQuery();
+  const [deleteUser] = useDeleteUserMutation();
+  const [updateUser] = useUpdateUserMutation(); // If this doesn't exist, you'll need to create it.
 
-  useEffect(() => {
-    if (data) {
-      const filtered = data.filter((item) => {
-        const itemData = `${item.firstName.toUpperCase()} ${item.lastName.toUpperCase()}`;
-        const searchData = search.toUpperCase();
-        return itemData.indexOf(searchData) > -1;
-      });
-      setFilteredData(filtered);
-    }
-  }, [search, data]);
+  const handleDelete = async (id) => {
+    await deleteUser(id).unwrap();
+    refetch();
+  };
 
-  const renderUserProfile = ({ item }) => (
-    <View style={styles.profileCardContainer}>
-      <TouchableOpacity
-        style={styles.profileCard}
-        onPress={() => navigation.navigate('UserInfo', { user: item })}
-      >
-        <Avatar
-          size="large"
-          rounded
-          source={{
-            uri: item.profilePicture || 'https://s.yimg.com/ny/api/res/1.2/PMsgs4BBYyt0dDLTaaevoA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/en/wrestle_zone_910/4017f03ebe9586c01755b4b3a524c81d',
-          }}
-          containerStyle={styles.avatarContainer}
-        />
-        <Text style={styles.name}>{`${item.firstName} ${item.lastName}`}</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  const handleEdit = (user) => {
+    navigation.navigate('UserForm', { user });
+  };
+
+  const handleNavigateToUserInfo = (user) => {
+    navigation.navigate('UserInfo', { user });
+  };
+
+  const handleNavigateToPostForm = () => {
+    navigation.navigate('PostForm'); // Use the correct screen name for PostForm.
+  };
 
   return (
-    <View style={styles.container}>
-      <SearchBar
-        placeholder="Search Users..."
-        onChangeText={setSearch}
-        value={search}
-        lightTheme
-        round
-      />
+    <View>
       {isLoading ? (
         <Text>Loading...</Text>
       ) : (
         <FlatList
-          data={filteredData}
+          data={data}
+          refreshControl={
+            <RefreshControl
+              refreshing={isLoading}
+              onRefresh={refetch}
+            />
+          }
+          renderItem={({ item }) => (
+            <ListItem
+              bottomDivider
+              onPress={() => handleNavigateToUserInfo(item)}
+            >
+              <ListItem.Content>
+                <ListItem.Title>{`${item.firstName} ${item.lastName}`}</ListItem.Title>
+              </ListItem.Content>
+              <Button
+                title="Edit"
+                onPress={() => handleEdit(item)}
+              />
+              <Button
+                title="Delete"
+                onPress={() => handleDelete(item.id)}
+              />
+            </ListItem>
+          )}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={renderUserProfile}
-          numColumns={2} // Adjust the number of columns for the grid layout
-          contentContainerStyle={styles.flatListContent}
         />
       )}
+      <Button title="Create Post" onPress={handleNavigateToPostForm} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 10,
-  },
-  profileCardContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    margin: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  profileCard: {
-    width: 150, // Fixed width for the card
-    height: 200, // Fixed height for the card
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 2,
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-  },
-  avatarContainer: {
-    marginBottom: 10, // Adjust as needed
-  },
-  name: {
-    marginTop: 10,
-    textAlign: 'center',
-  },
-  flatListContent: {
-    paddingVertical: 10,
-  },
-});
 
 export default UserList;
--- src/store/api/usersApi.ts
+++ src/store/api/usersApi.ts
@@ -3,76 +3,74 @@ import { db } from '../../../firebase-config';
 import { addDoc, doc, deleteDoc, collection, getDocs, updateDoc } from "firebase/firestore";
 
 const firebaseBaseQuery = async ({ baseUrl, url, method, body }) => {
-	switch (method) {
-		case 'GET':
-			const snapshot = await getDocs(collection(db, url));
-			const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-			return { data };
+    switch (method) {
+        case 'GET':
+            const snapshot = await getDocs(collection(db, url));
+            const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            return { data };
 
-		case 'POST':
-			const docRef = await addDoc(collection(db, url), body);
-			return { data: { id: docRef.id, ...body } };
+        case 'POST':
+            const docRef = await addDoc(collection(db, url), body);
+            return { data: { id: docRef.id, ...body } };
 
-		case 'DELETE':
-			const docDelRef = await deleteDoc(doc(db, url, body));
-			return { data: { id: docDelRef, } };
+            case 'DELETE':
+                // Ensure that 'body' is just the ID, not an object containing the ID.
+                await deleteDoc(doc(db, url, body));
+                return { data: { id: body } };
 
-		case 'PUT':
-			await updateDoc(doc(db, url, body.id), body);
-			return { data: { ...body } };
+            case 'PUT':
+                // The front-end should send 'body' as an object with an 'id' field and the fields to update.
+                const { id, ...updateFields } = body;
+                await updateDoc(doc(db, url, id), updateFields);
+                return { data: { id, ...updateFields } };
 
-		default:
-			throw new Error(`Unhandled method ${method}`);
-	}
+        default:
+            throw new Error(`Unhandled method ${method}`);
+    }
 };
 
 export const usersApi = createApi({
-  reducerPath: 'usersApi',
-	baseQuery: firebaseBaseQuery,
-	tagTypes: ['users'],
-	endpoints: (builder) => ({
-		// För att skapa en ny user. Anropas såhär createUser({ user: { firstName: firstName, lastName: lastName }})
-		createUser: builder.mutation({
-			query: ({ user }) => ({
-				baseUrl: '',
-				url: 'users',
-				method: 'POST',
-				body: user
-			}),
-			invalidatesTags: ['users']
-		}),
-		// För att hämta alla befintliga users
-		getUsers: builder.query({
-			query: () => ({
-				baseUrl: '',
-				url: 'users',
-				method: 'GET',
-				body: ''
-			}),
-			providesTags: ['users']
-		}),
-		// För att radera en user baserat på id. Anropas såhär: deleteUser(id)
-		deleteUser: builder.mutation({
-			query: (id) => ({
-				baseUrl: '',
-				url: 'users',
-				method: 'DELETE',
-				body: id
-			}),
-			invalidatesTags: ['users']
-		}),
-		// För att uppdatera en user. Anropas såhär updateUser({ user: { id: user.id, firstName: firstName, lastName: lastName }})
-		updateUser: builder.mutation({
-			query: ({ user }) => ({
-				baseUrl: '',
-				url: 'users',
-				method: 'PUT',
-				body: user
-			}),
-			invalidatesTags: ['users']
-		}),
-	}),
+    reducerPath: 'usersApi',
+    baseQuery: firebaseBaseQuery,
+    tagTypes: ['users'],
+    endpoints: (builder) => ({
+        createUser: builder.mutation({
+            query: ({ user }) => ({
+                baseUrl: '',
+                url: 'users',
+                method: 'POST',
+                body: user
+            }),
+            invalidatesTags: ['users']
+        }),
+        getUsers: builder.query({
+            query: () => ({
+                baseUrl: '',
+                url: 'users',
+                method: 'GET',
+                body: ''
+            }),
+            providesTags: ['users']
+        }),
+        deleteUser: builder.mutation({
+            query: (id) => ({
+                baseUrl: '',
+                url: 'users',
+                method: 'DELETE',
+                body: id
+            }),
+            invalidatesTags: ['users']
+        }),
+        updateUser: builder.mutation({
+            query: ({ user }) => ({
+                baseUrl: '',
+                url: 'users',
+                method: 'PUT',
+                body: user
+            }),
+            invalidatesTags: ['users']
+        }),
+    }),
 });
 
-// Exportera våra Queries och Mutations här.
 export const { useCreateUserMutation, useGetUsersQuery, useDeleteUserMutation, useUpdateUserMutation } = usersApi;
